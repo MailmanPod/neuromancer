4b45aa283f8649ab031ce0d1fe5fe246788e7219
Quality of Service <Quality of Service@192.168.1.104>
2012-12-01 13:40:42 -0300

    15º Commit: Aprobado Download de archivos RouterInput.

--- src/testing/FTPClientTesting.java
+++ src/testing/FTPClientTesting.java
@@ -21,90 +21,90 @@ import org.apache.commons.net.ftp.FTPFile;
  */
 public class FTPClientTesting {
 
-    private static void listFromServerFTP() throws Exception{
+    private static void listFromServerFTP() throws Exception {
         FTPClient client = new FTPClient();
-        
+
         client.setUseEPSVwithIPv4(true);
-        
+
         client.connect(getIPAddress());
         client.login("adminroot", "adminroot");
-        
+
         String[] names = client.listNames();
-        
-        for(String name: names){
+
+        for (String name : names) {
             System.out.println("Name: " + name);
         }
-        
+
         //FTPFile[] ftpFiles = client.listFiles();
         FTPFile[] ftpFiles = client.mlistDir("\\");
-        
-        for(FTPFile ff : ftpFiles){
-            if(ff.getType() == FTPFile.FILE_TYPE){
-                System.out.println("File Name: "+ ff.getName() + "  Type: " + ff.getType() 
+
+        for (FTPFile ff : ftpFiles) {
+            if (ff.getType() == FTPFile.FILE_TYPE) {
+                System.out.println("File Name: " + ff.getName() + "  Type: " + ff.getType()
                         + ";;;; " + FileUtils.byteCountToDisplaySize(ff.getSize()));
-            }else{
-                System.out.println("Directory Name: "+ ff.getName() + "  Type: " + ff.getType() 
+            } else {
+                System.out.println("Directory Name: " + ff.getName() + "  Type: " + ff.getType()
                         + ";;;; " + FileUtils.byteCountToDisplaySize(ff.getSize()));
             }
         }
         client.logout();
         client.disconnect();
     }
-    
-    private static void downloadFromServerFTP() throws Exception{
+
+    private static void downloadFromServerFTP() throws SocketException, IOException {
         FTPClient client = new FTPClient();
         FileOutputStream fos = null;
-        
+
         client.connect("localhost");
         client.login("adminroot", "adminroot");
-        
-        String local = ".\\FTPServer2\\RSAPublic.key";
+
+        String local = "D:\\Proyectos en NetBeans 9\\neuromancerV1\\FTPServer2\\RSAPublic.key";
         String remote = "RSAPublic.key";
-        
+
         fos = new FileOutputStream(local);
         client.retrieveFile(remote, fos);
-        
+
         client.noop();
-        
+
         fos.close();
         client.logout();
         client.disconnect();
     }
-    
-    private static void uploadToServerFTP() throws SocketException, IOException{
+
+    private static void uploadToServerFTP() throws SocketException, IOException {
         FTPClient client = new FTPClient();
         FileInputStream fis = null;
-        
+
         client.connect("localhost");
         client.login("adminroot", "adminroot");
-        
+
         String local = ".\\FTPServer2\\UPLOADER.txt";
         String remote = "UPLOADER.txt";
-        
+
         client.setFileTransferMode(FTP.BINARY_FILE_TYPE);
-        
+
         fis = new FileInputStream(local);
         client.storeFile(remote, fis);
-        
+
         client.noop();
-        
+
         fis.close();
         client.disconnect();
     }
-    
-    private static String getIPAddress() throws UnknownHostException{
+
+    private static String getIPAddress() throws UnknownHostException {
         InetAddress localhost = InetAddress.getByName("QoS-PC");
-        
+
         return localhost.getHostAddress();
     }
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        
-        listFromServerFTP();
-        //downloadFromServerFTP();
+
+        //listFromServerFTP();
+        downloadFromServerFTP();
         //uploadToServerFTP();
     }
 }
